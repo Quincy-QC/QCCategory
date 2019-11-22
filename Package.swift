c415ab9964e8ab12518ee8524631750e6040da85
UntilYou-QC <578184833@qq.com>
2019-11-22 17:11:54 +0800

    update

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "QCCategory",
             dependencies: []),
-        .testTarget(
-            name: "QCCategoryTests",
-            dependencies: ["QCCategory"]),
     ]
 )
